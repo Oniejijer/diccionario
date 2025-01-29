9751b1dca102ed90c26d37f06e01f9370b7b38fb
Oniejijer <javierjimenezr12@gmail.com>
2025-01-28 19:24:52 -0600

    Add files via upload

---
+++ bot_logic.py
@@ -0,0 +1,24 @@
+
+import random
+
+
+def gen_pass(pass_length):
+    elements = "qwertyuiopasdfghjklzxcvbnm+-/*!&$#?=@<>"
+    password = ""
+
+    for i in range(pass_length):
+        password += random.choice(elements)
+
+    return password
+
+
+def flip_coin():
+    flip = random.randint(0, 2)
+    if flip == 0:
+        return "HEADS"
+    else:
+        return "TAILS"
+    
+
+    
+    
